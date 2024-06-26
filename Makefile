3a96015dfd69159234708242e9bf35b84b21ff58
Christopher Chung <Cskywalker1@gmail.com>
2024-06-26 09:29:11 +0100

    Feat: add basic web scraper example

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ default: all
 all: scraper
 
 scraper: scraper.c
-	gcc -I./include -I./include/libxml2 scraper.c -o scraper
+	gcc -I./include -I./include/libxml2 scraper.c lib/libcurl.so -o scraper
 
 clean:
 	rm scraper
--- scraper.c
+++ scraper.c
@@ -1,11 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <curl/curl.h>
+#include <curl/typecheck-gcc.h>
 #include <libxml2/libxml/HTMLparser.h>
 #include <libxml2/libxml/xpath.h>
 
-int main(void) {
-	printf("Hello world\n");
-	return 0;
+struct CURLResponse
+{
+    char *html;
+    size_t size;
+};
+
+static size_t WriteHTMLCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    size_t realsize = size * nmemb;
+    struct CURLResponse *mem = (struct CURLResponse *)userp;
+    char *ptr = realloc(mem->html, mem->size + realsize + 1);
+
+    if (!ptr)
+    {
+        printf("Not enough memory available (realloc returned NULL)\n");
+        return 0;
+    }
+
+    mem->html = ptr;
+    memcpy(&(mem->html[mem->size]), contents, realsize);
+    mem->size += realsize;
+    mem->html[mem->size] = 0;
+
+    return realsize;
+}
+
+struct CURLResponse GetRequest(CURL *curl_handle, const char *url)
+{
+    CURLcode res;
+    struct CURLResponse response;
+
+    // initialize the response
+    response.html = malloc(1);
+    response.size = 0;
+
+    // specify URL to GET
+    curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+    // send all data returned by the server to WriteHTMLCallback
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteHTMLCallback);
+    // pass "response" to the callback function
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&response);
+    // set a User-Agent header
+    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36");
+    // perform the GET request
+    res = curl_easy_perform(curl_handle);
+
+    // check for HTTP errors
+    if (res != CURLE_OK)
+    {
+        fprintf(stderr, "GET request failed: %s\n", curl_easy_strerror(res));
+    }
+
+    return response;
+}
+
+int main()
+{
+    // initialize curl globally
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    // initialize a CURL instance
+    CURL *curl_handle = curl_easy_init();
+
+    // retrieve the HTML document of the target page
+    struct CURLResponse response = GetRequest(curl_handle, "https://www.scrapingcourse.com/ecommerce/");
+    // print the HTML content
+    printf("%s\n", response.html);
+
+    // scraping logic...
+
+    // cleanup the curl instance
+    curl_easy_cleanup(curl_handle);
+    // cleanup the curl resources
+    curl_global_cleanup();
+
+    return 0;
 }
